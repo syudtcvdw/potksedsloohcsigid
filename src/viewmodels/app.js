6ef8fbb08b65cd05284e7a468693acff110ad686
Banjo Mofesola Paul <mpdepaule1@gmail.com>
2017-07-04 20:21:30 +0100

    testing...

--- src/viewmodels/app.js
+++ src/viewmodels/app.js
@@ -112,6 +112,7 @@ var VM = new function () {
 
     function clientInfo() {
         let ci = this
+        console.log("NEW THIS")
 
         // observables
         ci.connected = ko.observable()
@@ -119,8 +120,9 @@ var VM = new function () {
         // subscriptions
         ci
             .connected
-            .subscribe(s => {
-                if (s) 
+            .subscribe(c => {
+                console.log(`client info connected: ${c}`)
+                if (c) 
                     vm.notify("Connection established")
                 else 
                     vm.notify("Connection to server lost", "error", {
