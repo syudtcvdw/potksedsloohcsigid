c074d9219b9d74d8fd07324d7a641dbd5e921e0e
Banjo Mofesola Paul <mpdepaule1@gmail.com>
2017-07-05 15:01:01 +0100

    Menu loaded dynamically now

--- src/viewmodels/components/control-frame.js
+++ src/viewmodels/components/control-frame.js
@@ -1,34 +1,67 @@
 var vm = function (params) {
     let vm = this
 
+    // props
+    let menuItems = [{id: 'admins', label: 'Admin profiles'}]
+
     // observables
     vm.schoolName = ko.observable() // what school is this
     vm.personName = ko.observable() // who's currently logged in
     vm.personEmail = ko.observable() // email address of currently logged in guy
     vm.disconnectionTime = ko.observable() // what time the connecion the server was lost
+    vm.menu = ko.observableArray() // what time the connecion the server was lost
 
     // computed
     vm.isServer = ko.computed(() => {
         return VM.MODE() == SERVER
     })
     vm.ipTooltip = ko.computed(() => {
-        if (!VM.connectionInfo()) return;
+        if (!VM.connectionInfo()) 
+            return;
         return vm.isServer()
             ? `Other workstations in your institution can connect to this server via this address: ${VM.IP()}`
-            : `You are currently${!VM.connectionInfo().connected()? ' NOT':''} connected to the Control Workstation at ${VM.IP()}`
+            : `You are currently${ !VM
+                .connectionInfo()
+                .connected()
+                ? ' NOT'
+                : ''} connected to the Control Workstation at ${VM.IP()}`
     })
 
     // subscriptions
     VM
         .ROLE
         .subscribe(() => _.defer(() => tooltip.refresh()))
-    vm.ipTooltip.subscribe(() => {
-        console.log('refreshing tooltip')
-        _.defer(() => tooltip.refresh())
-    })
+    vm
+        .ipTooltip
+        .subscribe(() => {
+            console.log('refreshing tooltip')
+            _.defer(() => tooltip.refresh())
+        })
+
+    // sub vm
+    function MenuItem(id, label) {
+        if (!id || !label) 
+            return;
+        let m = this
+
+        // observables
+        m.id = id
+        m.label = label
+        m.target = id + '-screen'
+
+        // computed
+        m.isActive = ko.computed(() => {
+            return m.target == VM.view()
+        })
+        m.icon = ko.computed(() => {
+            return `resx/icons/${m.id}${m.isActive()? '-inv':''}.png`
+        })
+    }
 
     // init
     VM.controlVm = vm
+    // load up menu
+    menuItems.map(m => vm.menu.push(new MenuItem(m.id, m.label)))
     _.defer(() => {
         $('control-frame').append("<script src='./imports/ext/tooltip.min.js'></script>")
         tooltip.setOptions({offsetDefault: 10})
