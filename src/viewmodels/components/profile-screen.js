3845f725cdbe8b95e118af9f6ea141f0af51159c
victor-iyiola <javafolabi@gmail.com>
2017-07-05 13:09:16 +0100

    profile-screen
    Sent update credentials to the socket

--- src/viewmodels/components/profile-screen.js
+++ src/viewmodels/components/profile-screen.js
@@ -1,7 +1,53 @@
-var vm = function (params) {
+const vm = function (params) {
 		let vm = this
 
-		// observables behaviors init
+		// observables
+		vm.updateName = ko.observable('')
+		vm.updatePwd = ko.observable('')
+		vm.updateErr = ko.observable()
+		vm.loading = ko.observable(false)
+		vm.seen = ko.observable(true)
+
+		// behaviors
+		vm.updateCreds = () => {
+				vm.loading(true)
+				if ( emptyFields(vm.updateName(), vm.updatePwd()))
+					vm.updateErr('Fill in all fields, pls.')
+				// send to the socket ( update profile )
+				VM.socket.emit('update profile', {
+					'name': vm.updateName(),
+					'password': vm.updatePwd()
+				}, (data) => {
+					if (data) {
+						VM.notify("Update successful!")
+						vm.seen(false)
+					} else 
+						vm.updateErr("Username/password incorrect!")
+				})
+				// change password
+				// retrieve first admin's password
+
+		}
+
+		vm.dismissLoading = () => {
+			vm.loading(false)
+			vm.updateErr(null)
+		}
+
+		vm.start = (data) => {
+			console.log('starting vm.')
+			console.log(data)
+		}
+
+		// helpers
+		function emptyFields(...fields) {
+				var empty = false
+				fields.forEach(items => {
+						if (items.trim().length === 0) 
+								empty = true
+				})
+				return empty
+		}
 
 }
 
