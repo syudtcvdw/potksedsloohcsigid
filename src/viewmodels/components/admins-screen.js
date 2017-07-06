0f7cb0eeeabbc12f23be645b802def456c00b842
victor-iyiola <javafolabi@gmail.com>
2017-07-06 11:27:26 +0100

    Adding new admin

--- src/viewmodels/components/admins-screen.js
+++ src/viewmodels/components/admins-screen.js
@@ -1,113 +1,114 @@
 const vm = function (params) {
-    let vm = this
+		let vm = this
 
-    vm.updateName = ko.observable()
-    vm.updatingProfile = ko.observable(false)
-    vm.admins = ko.observableArray()
-    vm.noAdmins = ko.observable(false)
-    vm.fetchingAdmins = ko.observable(false)
-    vm.newName = ko.observable()
-    vm.newEmail = ko.observable()
-    vm.newPwd = ko.observable()
-    vm.confNewPwd = ko.observable()
-    vm.addingAdmin = ko.observable(false)
+		vm.updateName = ko.observable()
+		vm.updatingProfile = ko.observable(false)
+		vm.admins = ko.observableArray()
+		vm.noAdmins = ko.observable(false)
+		vm.fetchingAdmins = ko.observable(false)
+		vm.newName = ko.observable()
+		vm.newEmail = ko.observable()
+		vm.newPwd = ko.observable()
+		vm.confNewPwd = ko.observable()
+		vm.addingAdmin = ko.observable(false)
 
-    // behaviors
-    vm.updateCreds = () => {
-        if (_anyEmpty(vm.updateName())) 
-            return VM.notify("Don't leave your name empty", 'warn'),
-            null
+		// behaviors
+		vm.updateCreds = () => {
+				if (_anyEmpty(vm.updateName())) 
+						return VM.notify("Don't leave your name empty", 'warn'),
+						null
 
-        vm.updatingProfile(true)
-        sockets.emit('update profile', { // send to the socket ( update profile )
-            'name': vm.updateName(),
-            'email': VM
-                .controlVm
-                .personEmail()
-        }, (data) => {
-            if (!data.status) 
-                VM.notify("Profile update failed, no reponse from Control Workstation", "error")
-            else {
-                if (data.response) {
-                    VM.controlVm.personName(data.response.name)
-                    VM.notify("Profile update successful")
-                    _.delay(vm.fetchAdmins, 500)
-                }
-                else 
-                    VM.notify("Profile update failed", "warn")
-            }
-            vm.updatingProfile(false)
-        })
-        // change password retrieve first admin's password
-    }
+				vm.updatingProfile(true)
+				sockets.emit('update profile', { // send to the socket ( update profile )
+						'name': vm.updateName(),
+						'email': VM
+								.controlVm
+								.personEmail()
+				}, (data) => {
+						if (!data.status) 
+								VM.notify("Profile update failed, no reponse from Control Workstation", "error")
+						else {
+								if (data.response) {
+										VM
+												.controlVm
+												.personName(data.response.name)
+										VM.notify("Profile update successful")
+										_.delay(vm.fetchAdmins, 500)
+								} else 
+										VM.notify("Profile update failed", "warn")
+						}
+						vm.updatingProfile(false)
+				})
+				// change password retrieve first admin's password
+		}
 
-    /**
+		/**
 	 * Delete admin
 	 */
-    vm.deleteAdmin = (admin) => {
-        // remove an admin
-    }
+		vm.deleteAdmin = (admin) => {
+				// remove an admin
+		}
 
-    /**
-	 * Add an admin
-	 */
-    vm.addAdmin = (admin) => {
-        // add an admin
-        if (admin) 
-            vm.adminDetails.push(admin)
+		/**
+		 * Add new admin
+		 */
+		vm.addAdmin = () => {
+			// add an admin
+			if ( _anyEmpty(vm.newName(), vm.newEmail(), vm.newPwd(), vm.confNewPwd()) )
+				VM.notify('Please fill in all fields.')
+			console.log('Okay. No empty')
+		}
 
-    }
+		/**
+		 * Fetches list of all admins from the server
+		 */
+		vm.fetchAdmins = () => {
+				vm.fetchingAdmins(true)
+				console.log('Fetching admins')
+				sockets.emit("get all admins", {}, data => {
+						vm.fetchingAdmins(false)
+						if (!data.status) 
+								vm.noAdmins(true)
+						else {
+								if (!data.response) 
+										vm.noAdmins(true)
+								else {
+										vm.noAdmins(false)
+										vm
+												.admins
+												.removeAll()
+										data
+												.response
+												.map(a => {
+														vm
+																.admins
+																.push(new Admin(a))
+												})
+								}
+						}
+				})
+		}
 
-    /**
-     * Fetches list of all admins from the server
-     */
-    vm.fetchAdmins = () => {
-        vm.fetchingAdmins(true)
-        console.log('Fetching admins')
-        sockets.emit("get all admins", {}, data => {
-            vm.fetchingAdmins(false)
-            if (!data.status)
-                vm.noAdmins(true)
-            else {
-                if (!data.response) 
-                    vm.noAdmins(true)
-                else {
-                    vm.noAdmins(false)
-                    vm
-                        .admins
-                        .removeAll()
-                    data
-                        .response
-                        .map(a => {
-                            vm
-                                .admins
-                                .push(new Admin(a))
-                        })
-                }
-            }
-        })
-    }
+		// sub-vm
+		function Admin(data) {
+				let a = this
 
-    // sub-vm
-    function Admin(data) {
-        let a = this
+				// observables
+				a.name = ko.observable(data.name || "")
+				a.email = ko.observable(data.email || "")
+				a.password = ko.observable(data.password || "")
+		}
 
-        // observables
-        a.name = ko.observable(data.name || "")
-        a.email = ko.observable(data.email || "")
-        a.password = ko.observable(data.password || "")
-    }
+		// local
+		function loadMyProfile() {
+				vm.updateName(VM.controlVm.personName())
+		}
 
-    // local
-    function loadMyProfile() {
-        vm.updateName(VM.controlVm.personName())
-    }
-
-    // init
-    vm.fetchAdmins()
-    loadMyProfile()
+		// init
+		vm.fetchAdmins()
+		loadMyProfile()
 }
 
 new Component('admins-screen')
-    .def(vm)
-    .load()
+		.def(vm)
+		.load()
