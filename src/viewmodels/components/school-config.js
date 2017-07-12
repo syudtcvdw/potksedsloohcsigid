6be80126b10333c8729a2b6d7f028b992dc6e614
Banjo Mofesola Paul <mpdepaule1@gmail.com>
2017-07-12 20:32:53 +0100

    Assessment metrics set

--- src/viewmodels/components/school-config.js
+++ src/viewmodels/components/school-config.js
@@ -22,6 +22,9 @@ var vm = function (params) {
     vm.termsPerSession = ko.observable()
     vm.currentTerm = ko.observable()
 
+    // assessment metrics
+    vm.metrics = new AssessmentMetrics()
+
     // states
     vm.logoChanged = ko.observable(false)
     vm.uiVisible = ko.observable(false)
@@ -187,6 +190,103 @@ var vm = function (params) {
         return newCurrTermList
     })
 
+    // sub vm
+    function AssessmentMetrics() {
+        let am = this
+
+        // observables
+        am.metrics = ko.observableArray()
+        am.updatingMetrics = ko.observable(false)
+
+        // behaviours
+        am.add = () => {
+            if (am.metrics().length > 0) {
+                let lm = am.metrics()[
+                    am
+                        .metrics()
+                        .length - 1
+                ]
+                if (_anyEmpty(lm.title(), lm.label(), lm.marks(), lm.compo())) 
+                    return VM.notify("Don't leave any detail out for the last metric", "warn"),
+                    null
+            }
+            am
+                .metrics
+                .push(new Metric())
+            redraw() // redraw layout
+        }
+        am.clear = () => {
+            VM.notify("Are you sure you want to clear all metrics, this action is not reversible", "black", {
+                proceed: () => {
+                    am
+                        .metrics
+                        .removeAll();
+                    redraw()
+                }
+            }, "clear metrics")
+        }
+        am.save = () => {
+            if (am.totalCompo() != 100) 
+                return VM.notify("Total percentage composition must be 100%", "error")
+            for (m of am.metrics()) {
+                if (_anyEmpty(m.title(), m.label(), m.marks(), m.compo())) 
+                    return VM.notify("Do not leave any field on the metrics empty", "error")
+                if (m.marks() < 1) 
+                    return VM.notify("The least obtainable score on any metric is 1", "error")
+                if (m.compo() < 1) 
+                    return VM.notify("Negative values not allowed for percentage composition", "error")
+                am.updatingMetrics(true)
+                sockets.emit('save assessment metrics', am.metrics(), data => {
+                    if (!data.status) 
+                        VM.notify("Problem saving assessment metrics, could not reach Control Workstation", "error")
+                    else {
+                        if (!data.response) 
+                            VM.notify("Unable to save assessment metrics", "error")
+                        else 
+                            VM.notify("Assessment metrics saved successfully")
+                    }
+                    am.updatingMetrics(false)
+                })
+            }
+            console.log(ko.toJSON(am.metrics()))
+        }
+
+        // computed
+        am.totalMarks = ko.computed(() => {
+            let total = 0
+            ko
+                .utils
+                .arrayForEach(am.metrics(), m => total += parseInt(m.marks() || 0))
+            return total
+        })
+        am.totalCompo = ko.computed(() => {
+            let total = 0
+            ko
+                .utils
+                .arrayForEach(am.metrics(), m => total += parseFloat(m.compo() || 0))
+            return parseFloat(total).toFixed(2)
+        })
+
+        // local
+        function Metric() {
+            let args = arguments.length > 0
+                ? arguments[0]
+                : {}
+            // props
+            console.log(args)
+            this.title = ko.observable(args.title || '') // title
+            this.label = ko.observable(args.label || '') // abbreviation
+            this.marks = ko.observable(args.marks || null) // marks obtainable
+            this.compo = ko.observable(args.compo || null) // percentage composition
+
+            // behaviours
+            this.kill = () => {
+                am.metrics.remove(this)
+                redraw()
+            }
+        }
+    }
+
     // local
     function dimens(max = 800) {
         let el = $('.school-logo')[0]
@@ -211,18 +311,29 @@ var vm = function (params) {
         this.name = name
     }
 
+    function redraw() {
+        _.defer(() => {
+            $
+                .fn
+                .matchHeight
+                ._update()
+            $('.school-config-screen,.metrics-list').scrollTop(10000)
+            tooltip.refresh()
+            $('#tooltip').remove() // because sometimes the tooltip litters the screen
+        }) // redraw layout
+    }
+
     // init
     _.defer(() => {
         // setup tooltips
         tooltip.refresh()
-        
+
         // when logo is loaded
-        $('.school-logo')
-            .on('load', function () {
-                // match heights because the profile pic card is the standard
-                $('.card').matchHeight()
-                vm.uiVisible(true)
-            })
+        $('.school-logo').on('load', function () {
+            // match heights because the profile pic card is the standard
+            $('.card').matchHeight()
+            vm.uiVisible(true)
+        })
 
         // set logo src
         fs.exists(USERDATA_ASSETS_PATH + 'logo.jpg', b => {
