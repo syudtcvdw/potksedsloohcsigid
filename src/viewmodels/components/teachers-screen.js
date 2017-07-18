3c80d218a6b56845e48a308ced37763c52fdbf29
victor-iyiola <javafolabi@gmail.com>
2017-07-18 17:54:10 +0100

    Edit teacher info

--- src/viewmodels/components/teachers-screen.js
+++ src/viewmodels/components/teachers-screen.js
@@ -1,148 +1,175 @@
 const vm = function (params) {
-    let vm = this
+  let vm = this
+
+  // props
+  let controlla
+  vm.info = "Click on a teacher's name to view their details, and manage them."
+
+  // observables
+  vm.teachers = ko.observableArray()
+  vm.newTeacher = ko.observable()
+  vm.confirmPassword = ko.observable()
+  vm.teacherDetail = ko.observable()
+  vm.newTeacherActionName = ko.observable()
+
+  // states
+  vm.connected = ko.observable(false)
+  vm.teachersFetchFailed = ko.observable(false)
+
+  // behaviours
+  vm.addTeacher = () => {
+    vm.newTeacherActionName('Add New Teacher')
+    vm.confirmPassword(null)
+    vm.newTeacher(new Teacher)
+    _tooltip()
+  }
+  vm.dismissAdd = () => {
+    vm.newTeacher(null)
+    _tooltip()
+  }
+  vm.loadTeachers = () => {
+    vm.teachersFetchFailed(false)
+    sockets.emit('get all teachers', null, data => {
+      if (!data.status) {
+        vm.teachersFetchFailed(true)
+        VM.notify("Unable to fetch teachers list, could not reach Control Workstation", "error", {
+          'try again': vm.loadTeachers
+        }, 'retry load teachers')
+      } else {
+        vm.teachersFetchFailed(false)
+        if (data.response) {
+          data
+            .response
+            .map(t => {
+              vm
+                .teachers
+                .push(new Teacher(t))
+            })
+          vm.connected(true)
+          _tooltip()
+        }
+      }
+    }, true)
+  }
+
+  // sub-vm
+  function Teacher() {
+    let t = this
+    let args = arguments.length > 0
+      ? arguments[0]
+      : {}
 
     // props
-    let controlla
-    vm.info = "Click on a teacher's name to view their details, and manage them."
-
-    // observables
-    vm.teachers = ko.observableArray()
-    vm.newTeacher = ko.observable()
-    vm.confirmPassword = ko.observable()
-    vm.teacherDetail = ko.observable()
+    t._id = ko.observable(args._id || '')
+    t.name = ko.observable(args.name || '')
+    t.email = ko.observable(args.email || '')
+    t.phone = ko.observable(args.phone || '')
+    t.password = ko.observable(args.password || '')
+    t.gender = ko.observable(args.gender || '')
+    t.addDate = ko.observable(args.addDate || null)
 
     // states
-    vm.connected = ko.observable(false)
-    vm.teachersFetchFailed = ko.observable(false)
+    t.saving = ko.observable(false)
 
     // behaviours
-    vm.addTeacher = () => {
-        vm.confirmPassword(null)
-        vm.newTeacher(new Teacher)
-        _tooltip()
+    t.save = () => {
+      console.log(t.name(), t.email())
+      if (_anyEmpty(t.name(), t.email(), t.phone(), _new
+        ? t.password()
+        : 'empty')) 
+        return VM.notify("Do not leave any detail empty", "warn")
+
+      let teacher = ko.toJS(t)
+      delete teacher.saving // not needed
+
+      if (_new) {
+        // add
+        if (t.password() != vm.confirmPassword()) 
+          return VM.notify("Passwords do not match, use the reveal buttons to comfirm", "error")
+
+        t.saving(true)
+        teacher.addDate = teacher.addDate
+          ? teacher.addDate
+          : _getUTCTime() / 1000 // to secs
+        delete teacher._id
+        sockets.emit('add teacher', teacher, data => {
+          if (!data.status) 
+            return t.saving(false),
+            VM.notify('Problem adding teacher, could not reach Control Workstation', 'error', {
+              'try again': t.save
+            }, 'retry add teacher')
+          else {
+            console.log(data)
+            if (typeof data.response == 'object') {
+              VM.notify('Teacher added successfully.')
+              vm
+                .teachers
+                .push(new Teacher(data.response))
+              vm.addTeacher()
+            } else if (data.response === false) 
+              VM.notify('Unable to add teacher', 'error')
+            else 
+              VM.notify(data.response, 'error')
+            t.saving(false)
+          }
+        })
+      } else {
+        // edit
+        if ( t.password() !== vm.confirmPassword() ) 
+          VM.notify("Passwords do not match, use the reveal buttons to confirm", "error")
+        t.saving(true)
+        sockets.emit('edit teacher', teacher, data => {
+          if ( !data.status )
+            return t.saving(false),
+            VM.notify('Problem editing teacher, could not reach Control Workstation', 'error', {
+              'try again': t.save
+            }, 'retry edit teacher')
+          else {
+            console.log(data)
+          }
+        })
+      }
     }
-    vm.dismissAdd = () => {
-        vm.newTeacher(null)
-        _tooltip()
+    t.open = () => {
+      vm.teacherDetail(new TeacherDetail(t))
+      controlla.next()
     }
-    vm.loadTeachers = () => {
-        vm.teachersFetchFailed(false)
-        sockets.emit('get all teachers', null, data => {
-            if (!data.status) {
-                vm.teachersFetchFailed(true)
-                VM.notify("Unable to fetch teachers list, could not reach Control Workstation", "error", {
-                    'try again': vm.loadTeachers
-                }, 'retry load teachers')
-            } else {
-                vm.teachersFetchFailed(false)
-                if (data.response) {
-                    data
-                        .response
-                        .map(t => {
-                            vm
-                                .teachers
-                                .push(new Teacher(t))
-                        })
-                    vm.connected(true)
-                    _tooltip()
-                }
-            }
-        }, true)
-    }
-
-    // sub-vm
-    function Teacher() {
-        let t = this
-        let args = arguments.length > 0 ?
-            arguments[0] : {}
-
-        // props
-        t._id = ko.observable(args._id || '')
-        t.name = ko.observable(args.name || '')
-        t.email = ko.observable(args.email || '')
-        t.phone = ko.observable(args.phone || '')
-        t.password = ko.observable(args.password || '')
-        t.gender = ko.observable(args.gender || '')
-        t.addDate = ko.observable(args.addDate || null)
-
-        // states
-        t.saving = ko.observable(false)
-
-        // behaviours
-        t.save = () => {
-            if (_anyEmpty(t.name(), t.email(), t.phone(), _new ?
-                    t.password() :
-                    'empty'))
-                return VM.notify("Do not leave any detail empty", "warn")
-
-            let teacher = ko.toJS(t)
-            delete teacher.saving // not needed
-
-            if (_new) {
-                // add
-                if (t.password() != vm.confirmPassword())
-                    return VM.notify("Passwords do not match, use the reveal buttons to comfirm", "error")
-
-                t.saving(true)
-                teacher.addDate = teacher.addDate ?
-                    teacher.addDate :
-                    _getUTCTime() / 1000 // to secs
-                delete teacher._id
-                sockets.emit('add teacher', teacher, data => {
-                    if (!data.status)
-                        return t.saving(false),
-                            VM.notify('Problem adding teacher, could not reach Control Workstation', 'error', {
-                                'try again': t.save
-                            }, 'retry add teacher')
-                    else {
-                        console.log(data)
-                        if (typeof data.response == 'object') {
-                            VM.notify('Teacher added successfully.')
-                            vm
-                                .teachers
-                                .push(new Teacher(data.response))
-                            vm.addTeacher()
-                        } else if (data.response === false)
-                            VM.notify('Unable to add teacher', 'error')
-                        else
-                            VM.notify(data.response, 'error')
-                        t.saving(false)
-                    }
-                })
-            }
-        }
-        t.open = () => {
-            vm.teacherDetail(new TeacherDetail(t))
-            controlla.next()
-        }
-        t.contextmenu = (o,e) => {
-            VM.contextmenu.prep(e).show({
-                'View': t.open,
-                'Edit profile': () => {}, // replace callback here with edit behaviour
-                'Delete': () => {} // replace callback here with delete behaviour
-            })
-        }
-
-        // init
-        let _new = !t._id()
-    }
-
-    function TeacherDetail(teacher) {
-        let t = this
-        if (!teacher) return
-        // props
-        t.me = teacher
-        // behaviours
+    t.contextmenu = (o, e) => {
+      VM
+        .contextmenu
+        .prep(e)
+        .show({
+          'View': t.open,
+          'Edit profile': () => {
+            vm.newTeacherActionName('Edit Teacher Details')
+            vm.confirmPassword(t.password())
+            vm.newTeacher(t)
+          }, // replace callback here with edit behaviour
+          'Delete': () => {} // replace callback here with delete behaviour
+        })
     }
 
     // init
-    _.defer(() => {
-        controlla = $('.sectionizr').sectionize()
-        vm.loadTeachers()
-    })
+    let _new = !t._id()
+  }
+
+  function TeacherDetail(teacher) {
+    let t = this
+    if (!teacher) 
+      return
+      // props
+    t.me = teacher
+    // behaviours
+  }
+
+  // init
+  _.defer(() => {
+    controlla = $('.sectionizr').sectionize()
+    vm.loadTeachers()
+  })
 
 }
 
 new Component('teachers-screen')
-    .def(vm)
-    .load()
+  .def(vm)
+  .load()
